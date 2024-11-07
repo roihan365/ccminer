acf54ba1e930089f2fbbe93cf3345fdbe12e56fb
roihan365 <bitcoiner.banjarmasin@gmail.com>
2024-11-07 20:25:58 +0800

    Create install-lib.sh

---
+++ install-lib.sh
@@ -0,0 +1,8 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+
+sudo chmod +x build.sh
+sudo chmod +x configure.sh
+sudo chmod +x autogen.sh
+sudo chmod +x ccminer
+
+sudo ./build.sh
